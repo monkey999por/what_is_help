0bcfac2ba7847da69164402f26b041979956622e
monkey999por <34706709+monkey999por@users.noreply.github.com>
2022-12-17 20:05:56 +0900

    auto deploy update!

--- draft_work/whatis_html_css/deploy.ps1
+++ draft_work/whatis_html_css/deploy.ps1
@@ -1,9 +1,9 @@
-cd $PSScriptRoot
-get httpd
-$apache = Get-Clipboard |Split-Path -Parent
+Set-Location $PSScriptRoot
+$apache_bin = (Get-Command httpd).Source 
+$apache = Split-Path -Parent $apache_bin | Split-Path -Parent
 $htdocs = Join-Path $apache htdocs
 $htdocs
-if (Test-Path $htdocs){
+if (Test-Path $htdocs) {
     Copy-Item -Path .\sample\* $htdocs -Force -Recurse
 }
 
