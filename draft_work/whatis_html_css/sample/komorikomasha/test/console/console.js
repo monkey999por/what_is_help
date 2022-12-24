6982180b675d5036c7f972a646cb2b4c5aec2425
monkey999por <34706709+monkey999por@users.noreply.github.com>
2022-12-25 02:25:15 +0900

    temp

--- draft_work/whatis_html_css/sample/komorikomasha/test/console/console.js
+++ draft_work/whatis_html_css/sample/komorikomasha/test/console/console.js
@@ -1,21 +1,55 @@
+const commandHistory = [];
+let keyPressCount = 0;
+
 document.addEventListener(
   "DOMContentLoaded",
   () => {
+    const server = document.querySelector(".server");
+    const user = document.querySelector(".user");
     const input = document.querySelector('input[type="text"]#command');
     const logs = document.querySelector(".logs");
 
+    server.textContent = "192.168.0.1";
+    user.textContent = "monkey999";
+
+    input.focus();
+
     input.addEventListener(
-      "keypress",
+      "keydown",
       (e) => {
-        if (e.keyCode === 13) {
-          runCommand((args) => {
-            console.log(args);
-            return "result dayo-";
-          });
-          input.value = "";
-          console.log(pBlock);
-          console.log(input.value);
+        // 上押下
+        if (e.keyCode === 38) {
+          if (commandHistory) {
+            if (commandHistory.length >= keyPressCount + 1) {
+              keyPressCount++;
+            }
+
+            const temp = commandHistory[commandHistory.length - keyPressCount];
+            input.value = temp ? temp : "";
+          }
+        } else if (e.keyCode === 40) {
+          // 下押下
+          if (
+            commandHistory.length >= keyPressCount - 1 &&
+            commandHistory.length <= keyPressCount - 1
+          ) {
+            keyPressCount--;
+          }
+          console.log(commandHistory.length - keyPressCount);
+          const temp = commandHistory[commandHistory.length - keyPressCount];
+          input.value = temp ? temp : "";
         }
+
+        // Enter押下時
+        if (e.keyCode === 13) {
+          keyPressCount = 0;
+          runCommand(ssh);
+          input.value = "";
+        }
+        // 一番下にスクロール
+        const element = document.documentElement;
+        const bottom = element.scrollHeight - element.clientHeight;
+        window.scroll(0, bottom);
       },
       false
     );
@@ -25,33 +59,59 @@ document.addEventListener(
      * @param commandFunc 実行するコマンド
      * @param args コマンドの引数
      */
-    const runCommand = (commandFunc, args) => {
+    const runCommand = (commandFunc) => {
+      // コマンド実行
+      const result = commandFunc(input.value);
+
+      // 履歴の保存
+      // 上ボタン、下ボタンで使う。
+      commandHistory.push(input.value);
+
+      // 結果生成
       // 実行したコマンド
-      const history = document.createElement("p");
-      const historyContent = document.createTextNode(
-        `server@user$ ${input.value}`
-      );
+      const historyElement = document.createElement("p");
+      historyElement.textContent = `${server.textContent}@${user.textContent}$ ${input.value}`;
 
       // コマンドの結果
-      const result = document.createElement("p");
-      const resultContent = document.createTextNode(commandFunc(args));
-      const cls = document.createAttribute("class");
-      cls.value = "hash";
-      result.setAttributeNode(cls);
+      const resultElement = document.createElement("p");
+      resultElement.textContent = result;
 
-      history.appendChild(historyContent);
-      result.appendChild(resultContent);
+      const group = document.createElement("div");
+      group.setAttribute("class", "histories");
 
-      const div = document.createElement("div");
-      div.appendChild(history);
-      div.appendChild(result);
-
-      logs.appendChild(div);
+      group.appendChild(historyElement);
+      group.appendChild(resultElement);
+      logs.appendChild(group);
     };
+
+    // 画面クリックでコマンド入力にフォーカス
+    document.addEventListener("click", () => input.focus(), false);
   },
   false
 );
 
-//どこでもクリックすればinputにカーソルする
-// inputのフォントサイズ
-// コマンドにonforcusでスタイル変更
+// const commands = [
+//   {
+//     name: "ssh",
+//     func: ssh,
+//   },
+// ];
+
+/**
+ * ssh mock
+ */
+const ssh = (args) => {
+  const sshFormat = /[0-9]{3}\.[0-9]{3}\.[0-9]{1,3}\.[0-9]{1,3}@.+/gi;
+  if (sshFormat.test(args)) {
+    console.log("ok");
+  } else {
+    console.log("ng");
+  }
+  const server = document.querySelector(".server");
+  const user = document.querySelector(".user");
+
+  return `login to ${args}`;
+};
+
+// 上ボタンで履歴をたどる
+// コマンドの一つ目で関数を特定
