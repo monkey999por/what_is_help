d9aa0a5cc48c5cce1c65e87ebca874077dddf35a
monkey999por <34706709+monkey999por@users.noreply.github.com>
2022-12-29 20:08:18 +0900

    temp

--- draft_work/whatis_html_css/sample/komorikomasha/bs-config.js
+++ draft_work/whatis_html_css/sample/komorikomasha/bs-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  files: "./test/**", //監視するファイルを指定
+  files: ["./main/**", ".main/images/*.jpg"], //監視するファイルを指定
   server: {
     //デフォルトで開くファイルを指定
-    baseDir: "./test/style_test/layout/", //ファイルまでのディレクトリのパス
-    index: "layout.html", //ファイル名
+    baseDir: "./main/portfolio/", //ファイルまでのディレクトリのパス
+    index: "01.html", //ファイル名
   },
   proxy: false,
   port: 8000, //使用するポート番号
