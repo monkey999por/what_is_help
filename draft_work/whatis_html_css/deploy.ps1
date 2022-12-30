5aef8f14f0f0a156d71a81c4f6d408763e4d0342
monkey999por <34706709+monkey999por@users.noreply.github.com>
2022-12-30 18:00:38 +0900

    auto create link

--- draft_work/whatis_html_css/deploy.ps1
+++ draft_work/whatis_html_css/deploy.ps1
@@ -4,7 +4,8 @@ $apache = Split-Path -Parent $apache_bin | Split-Path -Parent
 $htdocs = Join-Path $apache htdocs
 $htdocs
 if (Test-Path $htdocs) {
-    Copy-Item -Path .\sample\* $htdocs -Force -Recurse
+    Copy-Item -Path .\sample\komorikomasha\main\* $htdocs -Force -Recurse
+    Copy-Item -Path .\sample\komorikomasha\test\* $htdocs -Force -Recurse
 }
 
 
---
+++ draft_work/whatis_html_css/sample/komorikomasha/main/common/js/const.js
@@ -0,0 +1,3 @@
+export const TITLE = {
+  "/portfolio/01.html": "Cafe Debut",
+};
